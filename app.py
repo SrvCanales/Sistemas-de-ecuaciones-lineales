56e69198414da5010771d228025abb2756cffd89
SrvCanales <srvcanales@gmail.com>
2026-08-31 02:49:22 -0600

    Update file path to base.html in app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).parent
 HTML_FILE = BASE_DIR / "components" / "sistemas.html"
 
 
-with open(base.html, "r", encoding="utf-8") as f:
+with open(https://SrvCanales.github.io/Sistemas-de-ecuaciones-lineales/base.html, "r", encoding="utf-8") as f:
     html = f.read()
 
 components/base.html(
