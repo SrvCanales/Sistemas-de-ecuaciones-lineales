fccd1273f607e695dc10988c93792826e21616c8
SrvCanales <srvcanales@gmail.com>
2026-08-31 02:20:50 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from pathlib import Path
+
+st.set_page_config(
+    page_title="Guía - Sistemas de ecuaciones lineales",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
+#Rutas
+BASE_DIR = Path(__file__).parent
+HTML_FILE = BASE_DIR / "components" / "sistemas.html"
+
+
+with open(HTML_FILE, "r", encoding="utf-8") as f:
+    html = f.read()
+
+components.html(
+    html,
+    height=1000,
+    scrolling=False
+)
