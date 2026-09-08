a0b778fb63cd358ad8243b1732aaaba042a0c665
SrvCanales <srvcanales@gmail.com>
2026-09-08 10:29:23 -0600

    Add cache clearing functionality
    Clear cached data and resources on app start.

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from frontend.ai_matrix_solver import generar_pdf_con_gemini
 import os
 import time
 
+st.cache_data.clear()
+st.cache_resource.clear()
+
 st.set_page_config(
     page_title="Guía - Sistemas de ecuaciones lineales",
     layout="wide",
